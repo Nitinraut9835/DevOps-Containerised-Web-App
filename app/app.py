cff1a3999b33738ec08045d694204bc3505c572e
Nitin Raut <rautnitin309@gmail.com>
2026-03-14 20:29:41 +0100

    Day 12: Initial project structure with Flask API

---
+++ app/app.py
@@ -0,0 +1,37 @@
+from flask import Flask
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+DB_HOST = os.getenv("DB_HOST")
+
+@app.route("/")
+def home():
+    return "DevOps Project Running by Nitin Raut"
+
+@app.route("/health")
+def health():
+    return "Service is healthy"
+
+@app.route("/db")
+def db_test():
+    conn = psycopg2.connect(
+        host=DB_HOST,
+        database="devopsdb",
+        user="devops",
+        password="devops"
+    )
+
+    cur = conn.cursor()
+    cur.execute("SELECT version();")
+
+    db_version = cur.fetchone()
+
+    cur.close()
+    conn.close()
+
+    return f"PostgreSQL version: {db_version}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
